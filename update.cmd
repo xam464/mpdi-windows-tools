36569167d27b6324a6675ee55b32c2c68e829ca5
xam464 <S1134077@student.windesheim.nl>
2024-05-20 12:49:58 +0200

    Added Update script

---
+++ update.cmd
@@ -0,0 +1,32 @@
+runas /noprofile /user:mymachine\administrator
+winget upgrade --all --accept-package-agreements --accept-source-agreements
+@echo off
+::: Prepare le processeur de commande
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
+echo          _ _,---._
+echo       ,-','       `-.___
+echo      ^/-;'               `._
+echo     ^/^\ ^/          ._   _,'o ^\
+echo    ( ^/^\       _,--'^\,','"`. )
+echo     ^|^\      ,'o     ^\'    ^/ ^/^\
+echo     ^|      ^\        ^/   ,--'""`-.
+echo     :       ^\_    _ ^/ ,-'         `-._
+echo      ^\        `--'  ^/                )
+echo       `.  ^\`._    ,'     ________,','
+echo         .--`     ,'  ,--` __^\___,;'
+echo          ^\`.,-- ,' ,`_)--'  ^/`.,'
+echo           ^\( ;  ^| ^| )      (`- ^/
+echo             `--'^| ^|)       ^|- ^/
+echo               ^| ^| ^|        ^| ^|
+echo               ^| ^| ^|,.,-.   ^| ^|_
+echo               ^| `. ^/ ^/   )---`  )
+echo              _^|  ^/    ,',   ,-'
+echo             ,'^|_(    ^/-^<._,' ^|--,
+echo             ^|    `--'---.     ^\ ^/ ^\
+echo             ^|          ^/ ^\    ^/^\  ^\
+echo           ,-^---._     ^|  ^\  ^/  ^\  ^\
+echo        ,-'        ^\----'   ^\ ^/    ^\--`.
+echo       ^/            ^\              ^\   ^\
+PAUSE
+EXIT
